7dbcbc4ffff200b2ae3a7fd7243b5e64a24141a8
unknown <mesteromar20@gmail.com>
2021-12-14 21:50:27 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const url = "mongodb+srv://omar:omar@cluster0.sf0zs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+const app = express()
+
+mongoose.connect(url, {useNewUrlParser:true})
+const con = mongoose.connection
+
+con.on('open', () => {
+    console.log('connected...')
+})
+
+app.use(express.json())
+
+const alienRouter = require('./routes/aliens')
+app.use('/aliens',alienRouter)
+const blocRouter = require('./routes/blocs')
+app.use('/blocs',blocRouter)
+const salleRouter = require('./routes/salles')
+app.use('/salles',salleRouter)
+
+
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
